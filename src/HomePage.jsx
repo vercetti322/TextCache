9de1ae48db2d0f1de407cbc07b5ee4bde5b76cb1
vercetti322 <vercettitommy322@gmail.com>
2024-09-09 19:47:51 +0530

    about to deploy!

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { Heading, Flex, Center, Text } from '@chakra-ui/react';
 import PasteModal from './components/PasteModal.jsx';
