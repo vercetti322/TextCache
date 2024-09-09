449d78c669b1712aaddf40a0936d97acbd877758
vercetti322 <vercettitommy322@gmail.com>
2024-09-09 19:51:34 +0530

    about to deploy!

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure this is correct for your deployment
+  build: {
+    rollupOptions: {
+      input: './index.html', // Make sure this path is correct
+    },
+  },
 });
